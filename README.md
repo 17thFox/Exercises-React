45c66c79ce10006b4412c08c970acecfccfd2474
17thfox <elisabeta.vulpe@gmail.com>
2016-10-31 12:40:32 +0200

    First 2 exercises

---
+++ README.md
@@ -0,0 +1,4 @@
+# React Exercises 
+
+The file codecademy_exercises.js contains only some exercises from [Codecademy](https://www.codecademy.com/learn/react) selected by me.
+
---
+++ codecademy_exercises.js
@@ -0,0 +1,32 @@
+// 1. Simple output
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var pi = (
+  <div>
+    <h1>
+      PI, YALL!
+    </h1>
+    <p>
+      {Math.PI.toFixed(20)}
+    </p>
+  </div>
+);
+
+ReactDOM.render(
+	pi,
+	document.getElementById('app')
+);
+
+
+
+// 2. Accesing variables inside JSX expression
+
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var theBestString = 'tralalalala i am da best';
+
+ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
